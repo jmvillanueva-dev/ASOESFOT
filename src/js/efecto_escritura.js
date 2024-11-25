faf2baa8e6f903ee8429f6db92a1429dc900c257
jmvillanueva-dev <jhonny.villanueva@epn.edu.ec>
2024-11-25 17:33:22 -0500

    first commit

---
+++ src/js/efecto_escritura.js
@@ -0,0 +1,12 @@
+var typed = new Typed("#typed", {
+    strings: [
+        'Bienvenid@s Esfotdianos', 
+        'Noticias actuales', 
+        'Apoyo académico', 
+        'Cursos de refuerzo', 
+        'y mucho más....'],
+    typeSpeed: 60,
+    backDelay: 800,
+    startDelay: 1000,
+    loop: true,
+});
